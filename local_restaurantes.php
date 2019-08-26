756b3a9b653de58c0b81860a89af8d32c44940d0
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-08-26 17:48:39 -0300

    Update local_restaurantes.php

--- local_restaurantes.php
+++ local_restaurantes.php
@@ -2,11 +2,13 @@
     
         <ul>
         <li>Restaurante Universitário (Unipampa) - R$4,00 para alunos de Graduação e de Pós-Graduação Stricto Sensu, R$ 11,00 para comunidade geral.</li>
-        <li>Restaurante Butecão do Morais (próximo à Unipampa) </li>
-        <li>Restaurante Prodócimo (bairro, próximo à entrada da cidade, amplo) </li>
-        <li>Restaurante Tradição (Centro, pequeno) </li>
-        <li>Restaurante Butiquim (Centro, pequeno) </li>
-        
+        <li>Butecão do Morais (próximo à Unipampa, médio) </li>
+        <li>Ponto Certo (frente à Unipampa, pequeno) </li>
+        <li>Prodócimo (bairro, próximo à entrada da cidade, grande) </li>
+        <li>Tradição (Centro, grande) </li>
+        <li>Butiquim (Centro, pequeno) </li>
+        <li>PampaGrill (Centro, costuma ter fila) </li>
+        <li>Capri (Centro, costuma ter fila) </li>
         </ul>
 
     <br><br>
