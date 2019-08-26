9440b14c8657a028746b9a3914fb97f71570b28b
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-08-26 17:10:00 -0300

    Update local_restaurantes.php

--- local_restaurantes.php
+++ local_restaurantes.php
@@ -2,10 +2,11 @@
     
         <ul>
         <li>Restaurante Universitário (Unipampa) - R$4,00 para alunos de Graduação e de Pós-Graduação Stricto Sensu, R$ 11,00 para comunidade geral.</li>
-        <li>Restaurante Butecão do Morais (próximo à Unipampa). </li>
-        <li>Restaurante Tradição (Centro) </li>
-        <li>Restaurante Butiquim (Centro) </li>
-        <li>Restaurante Pampa Grill (Centro) </li>
+        <li>Restaurante Butecão do Morais (próximo à Unipampa) </li>
+        <li>Restaurante Prodócimo (bairro, próximo à entrada da cidade, amplo) </li>
+        <li>Restaurante Tradição (Centro, pequeno) </li>
+        <li>Restaurante Butiquim (Centro, pequeno) </li>
+        
         </ul>
 
     <br><br>
