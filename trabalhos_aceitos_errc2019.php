b8118ab7a6908c8e5835ea0d3432227ffba6a510
K D <kreutz@acm.org>
2019-08-31 02:11:52 -0300

    last updates done at Sat Aug 31 02:11:52 -03 2019

--- trabalhos_aceitos_errc2019.php
+++ trabalhos_aceitos_errc2019.php
@@ -1,123 +1,128 @@
-<h4>LISTA PROVISÓRIA DOS PAPERS ACEITOS</h4>
+<h4>LISTA DOS PAPERS ACEITOS</h4>
 <p>
 <br>
 <b>Análise de segurança no uso de contêineres Docker em máquinas virtuais</b>
 <br>
 <i>Kerolayne Oliveira, Charles Miers (Universidade do Estado de Santa Catarina - UDESC)</i>
+<a href="https://errc.sbc.org.br/2019/papers/oliveira2019anlise.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/oliveira2019anlise.pdf">[PDF]</a>
 <br>
 <br>
 <b>Análise do Desempenho de Heurísticas na Coleta de Informações de Telemetria In-Band</b>
 <br>
 <i>Ariel Góes de Castro, Francisco Vogt, Victor Hugo Lopes, Sérgio Rossi da Silva, Arthur Lorenzon (Universidade Federal do Pampa), Fabio Rossi (Instituto Federal Farroupilha), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/castro2019anlise.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/castro2019anlise.pdf">[PDF]</a>
 <br>
 <br>
 <b>Análise do Desempenho de VPNs nas Camadas de Rede e Enlace</b>
 <br>
 <i>Daniel Stasiak, Walter Priesnitz Filho (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/stasiak2019anlise.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/stasiak2019anlise.pdf">[PDF]</a>
 <br>
 <br>
 <b>Avaliação do uso de Smart Contracts para Sistema de Saúde Colaborativa</b>
 <br>
 <i>Vinícius Branco, Bruno Lippert, Henry Nunes (PUCRS), Roben Lunardi (Instituto Federal do Rio Grande do Sul - IFRS), Avelino Zorzo (PUCRS)</i>
+<a href="https://errc.sbc.org.br/2019/papers/branco2019avaliao.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/branco2019avaliao.pdf">[PDF]</a>
 <br>
 <br>
 <b>Definindo um Protocolo de Autenticação Utilizando Bluetooth Low Energy para Dispositivos no Conceito de IoT</b>
 <br>
 <i>Andre Eichner, Nathan da Silva, Tiago Antônio Rizzetti (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/eichner2019definindo.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/eichner2019definindo.pdf">[PDF]</a>
 <br>
 <br>
 <b>Firewalls em Redes Definidas por Software: Estado da Arte</b>
 <br>
 <i>Maurício Fiorenza, Diego Kreutz (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/fiorenza2019firewalls.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/fiorenza2019firewalls.pdf">[PDF]</a>
 <br>
 <br>
 <b>Fog Computing: análise de desempenho computacional</b>
 <br>
 <i>Leonardo Foletto, Ivania Fischer, Roseclea Medina, Eder Pereira (Universidade Federal de Santa Maria)</i>
+<a href="https://errc.sbc.org.br/2019/papers/foletto2019fog.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/foletto2019fog.pdf">[PDF]</a>
 <br>
 <br>
 <b>Gestão de Privacidade no Armazenamento de Dados do Paciente em Registros Médico-Hospitalares Eletrônicos</b>
 <br>
 <i>Fabio Demo, Walter Priesnitz Filho (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/demo2019gesto.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/demo2019gesto.pdf">[PDF]</a>
 <br>
 <br>
 <b>LAZE - Um poderoso analisador de logs do Squid</b>
 <br>
 <i>Natália Knob (IFRS), Luis Knob (Pontifícia Universidade Católica do Rio Grande do Sul - PUCRS), Eduardo Germano da Silva (Universidade Federal do Rio Grande do Sul - UFRGS), Cristian Cleder Machado (Universidade Regional Integrada (URI) - Câmpus de Frederico Westphalen), Diego Lusa(IFRS - Campus Sertão)</i>
-<br>
-<br>
-<b>O Impacto Sustentável das Redes Móveis Densas Heterogêneas no Futuro 5G - Levantamento Bibliográfico</b>
-<br>
-<i>Luis Eduardo Simioni Centeleghe, Luis Felipe dos Santos Vasconcelos (Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul - Campus Canoas)</i>
+<a href="https://errc.sbc.org.br/2019/papers/knob2019laze.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/knob2019laze.pdf">[PDF]</a>
 <br>
 <br>
 <b>Plataforma e-consumo: Monitoramento Inteligente do Consumo de Energia Elétrica em Residências</b>
 <br>
 <i>Larissa Del Rio, Andre Eichner, Tiago Antônio Rizzetti (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/rio2019plataforma.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/rio2019plataforma.pdf">[PDF]</a>
 <br>
 <br>
 <b>Projeto redes: implementação de uma MANET para a conexão de dispositivos IoT utilizando protocolo OLSR</b>
 <br>
 <i>Joao Mário Brezolin, Cristiano Nunes (Instituto Federal Sul-Rio-Grandense - IFSul)</i>
+<a href="https://errc.sbc.org.br/2019/papers/brezolin2019projeto.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/brezolin2019projeto.pdf">[PDF]</a>
 <br>
 <br>
 <b>Proposta de Monitoramento e Gerenciamento Inteligente de Temperatura em Datacenters</b>
 <br>
 <i>Djalma Teixeira (Sociedade Educacional Três de Maio - SETREM), Adriano Vogel (PUCRS), Dalvan Griebler (PUCRS/SETREM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/teixeira2019proposta.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/teixeira2019proposta.pdf">[PDF]</a>
 <br>
 <br>
-<b>Segurança na camada de enlace</b>
-<br>
-<i>Felipe Franz (Universidade Tecnológica Federal do Paraná)</i>
-<br>
-<br>
-<b>Um Algoritmo de Balanceamento de Carga para a Computação em Névoa</b>
-<br>
-<i>Eder Pereira, Moisés Oliveira, Ivania Fischer (UFSM), Edson Luiz Padoin (UNIJUI/UFRGS), Roseclea Medina (Universidade Federal de Santa Maria - UFSM)</i>
-<br>
-<br>
-<b>Uma Comparação dos Dados da Ferramenta Zabbix com a Percepção dos Usuários da Rede no Instituto Federal Catarinense ? Campus Avançado Sombrio</b>
+<b>Uma Comparação dos Dados da Ferramenta Zabbix com a Percepção dos Usuários da Rede no Instituto Federal Catarinense - Campus Avançado Sombrio</b>
 <br>
 <i>Liliana Colares, Matheus Braga, Victor Martins de Sousa, Diuliana da Rosa (Instituto Federal Catarinense - Campus Sombrio - IFC Sombrio)</i>
+<a href="https://errc.sbc.org.br/2019/papers/colares2019uma.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/colares2019uma.pdf">[PDF]</a>
 <br>
 <br>
 <b>Zabbix Monitoring Tool Optimization at Hospital Universitário de Santa Maria</b>
 <br>
 <i>Vinícius Röpke, Matheus Guarienti, Tiago Antonio Rizzetti (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/ropke2019zabbix.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/ropke2019zabbix.pdf">[PDF]</a>
 <br>
 <br>
 </p>
-<h4>LISTA PROVISÓRIA DOS RESUMOS ESTENDIDOS ACEITOS</h4>
+<h4>LISTA DOS RESUMOS ESTENDIDOS ACEITOS</h4>
 <p>
 <br>
 <b>Avaliação de Desempenho de Heavy Hitters utilizando P4 e XDP</b>
 <br>
 <i>Douglas Machado, Ariel Góes de Castro, Francisco Vogt, Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/machado2019avaliao.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/machado2019avaliao.pdf">[PDF]</a>
 <br>
 <br>
 <b>Composição Eficiente de Encadeamento de Funções de Serviços</b>
 <br>
 <i>Igor Capeletti (Universidade Federal do Pampa - UNIPAMPA), Fabio Rossi (Instituto Federal Farroupilha - Campus Alegrete), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/capeletti2019composio.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/capeletti2019composio.pdf">[PDF]</a>
 <br>
 <br>
 <b>Registro de eventos de alocação de contêineres em Blockchain</b>
 <br>
 <i>Marco Marques, Charles Miers (Universidade do Estado de Santa Catarina - UDESC)</i>
+<a href="https://errc.sbc.org.br/2019/papers/marques2019registro.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/marques2019registro.pdf">[PDF]</a>
 <br>
 <br>
 <b>Rumo à visibilidade total da rede: uma estratégia adaptativa para coordenadar a coleta de dados de telemetria in-band</b>
 <br>
 <i>Rumenigue Hohemberger, Fabio Rossi (Instituto Federal Farroupilha - Campus Alegrete), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/hohemberger2019rumo.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/hohemberger2019rumo.pdf">[PDF]</a>
 <br>
 <br>
 <b>Rumo à Delegação Escalável e Segura de Gerenciamento de LANs para ISPs</b>
 <br>
 <i>Daniel Temp, Rodrigo Mansilha, (Universidade Federal do Pampa - UNIPAMPA), Deivid Rodrigues (Interneith), Diego Kreutz (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/temp2019rumo.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/temp2019rumo.pdf">[PDF]</a>
 <br>
 <br>
 <b>Uma Investigação sob a Percepção dos Usuários no Desempenho da Rede no Instituto Federal Catarinense - Campus Avançado Sombrio</b>
 <br>
 <i>Diuliana de Matos da Rosa, Liliana Campos Colares, Matheus Lorenzato Braga, Victor Martins de Sousa (Instituto Federal Catarinense - Campus Avançado Sombrio)</i>
+<a href="https://errc.sbc.org.br/2019/papers/colares2019uma.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/colares2019uma.pdf">[PDF]</a>
 <br>
 <br>
 </p>
@@ -127,16 +132,19 @@
 <b>Avaliação do uso de Smart Contracts para Sistema de Saúde Colaborativa [média geral no JEMS: 8.7]</b>
 <br>
 <i>Vinícius Branco, Bruno Lippert, Henry Nunes (PUCRS), Roben Lunardi (Instituto Federal do Rio Grande do Sul - IFRS), Avelino Zorzo (PUCRS)</i>
+<a href="https://errc.sbc.org.br/2019/papers/branco2019avaliao.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/branco2019avaliao.pdf">[PDF]</a>
 <br>
 <br>
 <b>Análise do Desempenho de Heurísticas na Coleta de Informações de Telemetria In-Band [média geral no JEMS: 8.6]</b>
 <br>
 <i>Ariel Góes de Castro, Francisco Vogt, Victor Hugo Lopes, Sérgio Rossi da Silva, Arthur Lorenzon (Universidade Federal do Pampa), Fabio Rossi (Instituto Federal Farroupilha), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
+<a href="https://errc.sbc.org.br/2019/papers/castro2019anlise.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/castro2019anlise.pdf">[PDF]</a>
 <br>
 <br>
 <b>Plataforma e-consumo: Monitoramento Inteligente do Consumo de Energia Elétrica em Residências [média geral no JEMS: 8.1]</b>
 <br>
 <i>Larissa Del Rio, Andre Eichner, Tiago Antônio Rizzetti (Universidade Federal de Santa Maria - UFSM)</i>
+<a href="https://errc.sbc.org.br/2019/papers/rio2019plataforma.bib">[BibTeX]</a><a href="https://errc.sbc.org.br/2019/papers/rio2019plataforma.pdf">[PDF]</a>
 <br>
 <br>
 </p>
