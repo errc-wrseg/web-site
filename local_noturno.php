1cc22fdc3c4092f12a496bc9e453f33e67e73346
K D <kreutz@acm.org>
2019-09-02 03:34:20 -0300

    last updates done at Mon Sep  2 03:34:20 -03 2019

--- local_noturno.php
+++ local_noturno.php
@@ -1,23 +1,23 @@
 <h3>Opções noturnas:</h3>
 <ul>
 <li><a href="https://duckduckgo.com/?q=restaurantes+alegrete-rs&t=ffab&atb=v130-1&ia=places">Bistrô Alegrete</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5341319-Reviews-Pizzaria_Da_Vinci-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizzaria Da Vinci</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d6628598-Reviews-La_Parrilla_Restaurante-Alegrete_State_of_Rio_Grande_do_Sul.html">La Parrilla Restaurante</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d6472994-Reviews-Primeira_Opcao_Pastelaria_e_Pizzaria-Alegrete_State_of_Rio_Grande_do_Sul.html">Primeira Opcao Pastelaria e Pizzaria</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5356339-Reviews-Joy_Boteco-Alegrete_State_of_Rio_Grande_do_Sul.html">Joy Boteco</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d11858539-Reviews-Me_Gusta_Parrilla-Alegrete_State_of_Rio_Grande_do_Sul.html">Me Gusta Parrilla</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d3259598-Reviews-Restaurante_e_Churrascaria_Prodocimo-Alegrete_State_of_Rio_Grande_do_Sul.html">Restaurante Prodócimo</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d7870146-Reviews-Restaurante_Bistro-Alegrete_State_of_Rio_Grande_do_Sul.html">Restaurante Bistro</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5742029-Reviews-Papa_Gula-Alegrete_State_of_Rio_Grande_do_Sul.html">Papa Gula</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d7597291-Reviews-Point_Do_Pastel-Alegrete_State_of_Rio_Grande_do_Sul.html">Point Do Pastel</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5746332-Reviews-Pizza_Tube-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizza Tube</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d13707969-Reviews-Pizzaria_Santo_Antonio-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizzaria Santo Antônio</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d4527424-Reviews-Pizza_Da_Pedra-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizza Da Pedra</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d12349364-Reviews-Subway-Alegrete_State_of_Rio_Grande_do_Sul.html">Subway</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d15231855-Reviews-Haru_Sushi_Bar-Alegrete_State_of_Rio_Grande_do_Sul.html">Haru Sushi Bar</a></li>
-<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d8498687-Reviews-Sushi_House_Temakeria-Alegrete_State_of_Rio_Grande_do_Sul.html">Sushi House Temakeria</a></li>
 <li><a href="https://www.foodyas.com/BR/Alegrete/1490873194490153/Casar%C3%A3o-Pizzaria">Casarão Pizzaria</a></li>
-<li><a href="https://www.foodyas.com/BR/Alegrete/160867997726234/Villa-Burger">Villa Burger</a></li>
 <li><a href="https://www.foodyas.com/BR/Alegrete/396570317437472/Doce-Vida-Steakhouse">Doce Vida Steakhouse</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d15231855-Reviews-Haru_Sushi_Bar-Alegrete_State_of_Rio_Grande_do_Sul.html">Haru Sushi Bar</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5356339-Reviews-Joy_Boteco-Alegrete_State_of_Rio_Grande_do_Sul.html">Joy Boteco</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d6628598-Reviews-La_Parrilla_Restaurante-Alegrete_State_of_Rio_Grande_do_Sul.html">La Parrilla Restaurante</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d11858539-Reviews-Me_Gusta_Parrilla-Alegrete_State_of_Rio_Grande_do_Sul.html">Me Gusta Parrilla</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5742029-Reviews-Papa_Gula-Alegrete_State_of_Rio_Grande_do_Sul.html">Papa Gula</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d4527424-Reviews-Pizza_Da_Pedra-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizza Da Pedra</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5746332-Reviews-Pizza_Tube-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizza Tube</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d5341319-Reviews-Pizzaria_Da_Vinci-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizzaria Da Vinci</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d13707969-Reviews-Pizzaria_Santo_Antonio-Alegrete_State_of_Rio_Grande_do_Sul.html">Pizzaria Santo Antônio</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d7597291-Reviews-Point_Do_Pastel-Alegrete_State_of_Rio_Grande_do_Sul.html">Point Do Pastel</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d6472994-Reviews-Primeira_Opcao_Pastelaria_e_Pizzaria-Alegrete_State_of_Rio_Grande_do_Sul.html">Primeira Opcao Pastelaria e Pizzaria</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d7870146-Reviews-Restaurante_Bistro-Alegrete_State_of_Rio_Grande_do_Sul.html">Restaurante Bistro</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d3259598-Reviews-Restaurante_e_Churrascaria_Prodocimo-Alegrete_State_of_Rio_Grande_do_Sul.html">Restaurante Prodócimo</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d12349364-Reviews-Subway-Alegrete_State_of_Rio_Grande_do_Sul.html">Subway</a></li>
+<li><a href="https://www.tripadvisor.com.br/Restaurant_Review-g2344057-d8498687-Reviews-Sushi_House_Temakeria-Alegrete_State_of_Rio_Grande_do_Sul.html">Sushi House Temakeria</a></li>
+<li><a href="https://www.foodyas.com/BR/Alegrete/160867997726234/Villa-Burger">Villa Burger</a></li>
 </ul>
 <br><br>
