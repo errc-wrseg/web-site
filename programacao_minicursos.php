c71c752004396520239eb20cb693d13bf4e6eb6b
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-09-05 12:09:16 -0300

    programacao - salas

--- programacao_minicursos.php
+++ programacao_minicursos.php
@@ -10,7 +10,7 @@
     
 
     <h3>Minicurso 2: Introdução à Propriedades Avançadas de Segurança da Informação</h3>
-    <p><b>Data:</b> 17/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> Laboratório 102</p>
+    <p><b>Data:</b> 17/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> DTIC</p>
     <p><b>Autores:</b> Diego Kreutz, Sabrina Carlé Winckler, Rodrigo de Oliveira Barbosa, João Otávio Chervinski, Tadeu Jenuário (UNIPAMPA)</p>
      <p>
         <b>Resumo:</b> Em protocolos de segurança tradicionais, assumimos que não há mais o que fazer se a chave secreta, compartilhada entre os usuários Alice e Bob, que estão trocando mensagens entre si, for comprometida. De fato, se considerarmos apenas as propriedades básicas de segurança (e.g. confidencialidade, integridade e autenticidade), tanto comunicações passadas quanto futuras serão comprometidas no momento em que a chave secreta for comprometida. Entretanto, existem propriedades e características avançadas de segurança, como Perfect Forward Secrecy (PFS), Post-Compromise Recovery (PCR) e Post-Compromise Secure (PCS) que permitem a construção de protocolos de segurança mais resilientes ao comprometimento de chaves secretas. Enquanto PFS protege comunicações passadas, PCR e PCS têm por objetivo recuperar o protocolo a um estado seguro e proteger comunicações futuras, respectivamente. Adicionalmente, outra propriedade desejada no projeto de protocolos atuais e futuros é a PQS (Post-Quantum Secure). Esta propriedade assegura que os protocolos não serão alvos fáceis de computadores quânticos, que são capazes de quebrar a segurança de protocolos tradicionais, como o RSA e o Diffie-Hellman, utilizando apenas ataques de força-bruta. Como estas propriedades de segurança são pouco exploradas na literatura, ou recentes como é o caso da PCR, PCS e PQS, neste minicurso o nosso objetivo é apresentar uma visão geral, de forma didática e progressiva, de propriedades básicas e avançadas de segurança no projeto de protocolos e sistemas mais robustos. Além disso, nós apresentaremos exemplos reais de protocolos e sistemas que utilizam as diferentes propriedades de segurança discutidas. Finalmente, iremos também demonstrar, de forma simples e didática, a importância da utilização de ferramentas formais para verificação automática de protocolos de segurança, como os apresentados durante o minicurso.
@@ -20,7 +20,7 @@
             
     
     <h3>Minicurso 3: Introdução à linguagem de programação P4, o futuro das redes</h3>
-    <p><b>Data:</b> 18/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> Laboratório 102</p>
+    <p><b>Data:</b> 18/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> DTIC</p>
     <p><b>Autores:</b> Alisson B. Zanetti, Pedro E. Camera, Marco A. Trentin, Ricardo O. Schmidt (UPF)</p>
     <p>
     <b>Resumo:</b>As redes de computadores tradicionais enfrentam grandes desafios de manutenibilidade frente às demandas atuais por conexões rápidas e dinâmicas. O crescente uso de recursos de rede nos seus mais variados níveis, aliado ao privilégio de configurações por parte dos fabricantes, tornaram o emprego das arquiteturas conhecidas uma pra ́tica engessada, complexa e pouco fértil para novas aplicações. Utilizando-se de uma compreensão que separa os módulos de controle das demais funções de rede, definindo-os por meio de software, o paradigma SDN quebra a integração vertical entre a gerência de rede e a camada por onde os dados trafegam, proporcionando o controle centralizado dos equipamentos, abstraindo muito da complexidade empregada nos ambientes de rede, quer por políticas dos fabricantes, quer pela natureza da arquitetura tradicional. Este minicurso tem como objetivo demonstrar o funcionamento destas novas tecnologias, com ênfase na programação do plano de dados por meio da linguagem P4, utilizando-se de ambientes simulados e manipulados pelo próprio cursista, compreendendo assim o imenso universo de possibilidades a serem exploradas nesta nova dinâmica por trás das redes futuras. 
@@ -30,7 +30,7 @@
     
         
     <h3>Minicurso 4: Análise de tráfego de máquinas virtuais na rede de controle de nuvens computacionais baseadas em OpenStack</h3>
-    <p><b>Data:</b> 18/09/2019 <b>Horário:</b> 14:30-16:00, 16:30-18:00 <b>Local:</b> Laboratório 102</p>
+    <p><b>Data:</b> 18/09/2019 <b>Horário:</b> 14:30-16:00, 16:30-18:00 <b>Local:</b> DTIC</p>
     <p><b>Autores:</b> Adnei Willian Donatti, Charles Christian Miers, Guilherme Piêgas Koslovski, Maurício Aronne Pillon (UDESC)</p>
     <p>
     <b>Resumo:</b>A computação em nuvem é um paradigma para disponibilização de infraestrutura computacional. Este paradigma muda a localização da infraestrutura para a rede, no sentido de possibilitar a distribuição ou realocação de carga entre os diversos recursos computacionais conectados a rede. Portanto, emprega-se virtualização para reduzir os custos relacionados com o gerenciamento e manutenção de recursos de hardware e software [Vaquero et al. 2008, Mell and Grance 2011]. Segundo [Jadeja and Modi 2012], o usuário pode ter acesso a uma infraestrutura virtualizada externa mediante pagamento pelo uso, ou com a implantação de uma nuvem privada, cuja utilização alinha-se às necessidades de uma organização. Segundo [Wang and Ng 2010], questões relacionadas ao desempenho de uma nuvem dependem da identificação de comportamentos e operações que ocorrem durante o seu uso. Além disso, percebe-se, que há uma necessidade de identificar operações, muitas das quais são operações que ocorrem no nível da camada de rede e demandam análise e compreensão do que está trafegando e sua finalidade. Neste sentido, a caracterização de tráfego auxilia este entendimento, por meio do emprego de técnicas e métodos que possibilitam a coleta e identificação de forma sistematizada. Apesar de não ter sido identificada uma definição formal ou me ́todo para caracterização de tráfego, este termo refere-se à minuciosa análise do tráfego de uma rede, buscando entender seu comportamento e também as consequências que derivam deste comportamento. 
@@ -39,7 +39,7 @@
     
 
     <h3>Minicurso 5: Web Application Firewalls (WAFs): Teoria e Prática</h3>
-    <p><b>Data:</b> 19/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> Laboratório 102</p>
+    <p><b>Data:</b> 19/09/2019 <b>Horário:</b> 09:00-10:30, 11:00-12:30 <b>Local:</b> DTIC</p>
     <p><b>Autores:</b> Felipe Melchior, Diego Kreutz, Mauricio Fiorenza, Fernando Flora (UNIPAMPA), Douglas Macedo (UFSC)</p>
     
     <p>
